f2ad335058ccdd703f9df59c9fcd82d89694f09e
lukaszkwinta <lukaszmind@gmail.com>
2022-07-22 23:45:55 +0200

    Test

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ const int PIXELS_COUNT_Y = 28;
 
 const int PIXEL_SIZE_X = 15;
 const int PIXEL_SIZE_Y = 15;
-ffsfsf
+
 int main() {
     App app = App("Number Recognition", PIXELS_COUNT_X, PIXELS_COUNT_Y, PIXEL_SIZE_X, PIXEL_SIZE_Y);
     app.run();
